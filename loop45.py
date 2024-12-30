3c7f90362ec4d4aab73226c882ecad8999b1e580
Aditya Budhathoki <budhathokiaditya21@gmail.com>
2024-12-30 09:20:47 +0545

    Update loop45.py

--- loop45.py
+++ loop45.py
@@ -1,13 +1,11 @@
-def is_prime(number):
-    if number <= 1:
-        print("false")
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0: 
-           print("false")
-    else:
-        True 
 number = int(input("Enter a number: "))
-if is_prime(number):
-    print(f"{number} is a prime number.")
-else:
+
+if number <= 1:
     print(f"{number} is not a prime number.")
+else:
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            print(f"{number} is not a prime number.")
+            break
+    else:
+        print(f"{number} is a prime number.")
